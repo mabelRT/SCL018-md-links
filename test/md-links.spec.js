ea502562035a5fc6cc8d00c7d7c5268d8b6c27d2
mabel <wendy.torrico.wt@gmail.com>
2022-01-11 08:00:25 -0300

    provando test

--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,1 +1,20 @@
+import { extentFile, mdLinks } from '../index.js';
+
+
+describe('extentFile', () => {
+  it("debería retornar un objeto", () => {
+    const path = "test.md";
+    const result = mdLinks(path);
+    expect(result).toBeInstanceOf(Object);
+  });
+});
+
+
+describe('mdLinks', () => {
+  it("debería retornar una promesa", () => {
+    const path = "test.md";
+    const result = mdLinks(path);
+    expect(result).toBeInstanceOf(Promise);
+  });
+});
 
