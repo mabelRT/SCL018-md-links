ac656e44fe029bc7c09d7300b74c97194b0075b6
mabel <wendy.torrico.wt@gmail.com>
2022-01-11 07:57:21 -0300

    funcion extraer links

--- index.js
+++ index.js
@@ -1,8 +1,47 @@
-import fs from 'fs';
-import path from 'path';
-import fetch from 'node-fetch';
+import fs from "fs";
+import path from "path";
+import Yargs from "yargs";
 
 
-module.exports = () => {
-  // ...
-};
+const userPath = process.argv[2];
+const option = Yargs(process.argv.slice(2)).argv;
+const regularEx = /\[([^\]]+)]\((https?:\/\/[^\s)]+)\)/g;
+
+
+export const extentFile = (file)=>{
+    const ext = path.extname(file);
+    if(ext === '.md'){
+        return readFile(file);
+    }else{ console.log('No es un archivo md')}
+}
+
+//función para leer documento
+export const readFile = (extentFile)=>{
+    try{
+        if(fs.existsSync(extentFile)){
+        const data = fs.readFileSync(extentFile, 'utf8')
+        
+        return readLinks(data)
+    };
+        
+    }catch(err){
+        console.log(err)
+    }
+}
+
+
+//Función jefe
+export const mdLinks = (file)=>{
+    return new Promise((resolve, reject)=>{
+        const links = extentFile(file);
+        if(option.validate){
+            resolve(validateLinks(links));
+        }else{
+            resolve(links);
+        }
+        reject();
+        
+    })
+}
+
+mdLinks(userPath).then((results)=> console.table(results));
